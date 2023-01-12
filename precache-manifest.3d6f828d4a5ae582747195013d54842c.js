a86daea5919301e5ed3b139c558ca875f738280c
a6uxa4 <a6uxa4@icloud.com>
2023-01-12 19:30:30 +0600

    Updates

---
+++ precache-manifest.3d6f828d4a5ae582747195013d54842c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16c6e2a1fd23540342e5ae864e78b17",
+    "url": "/Pomodoro-Redux/index.html"
+  },
+  {
+    "revision": "0cc7635c6b6997750031",
+    "url": "/Pomodoro-Redux/static/css/main.ed97085d.chunk.css"
+  },
+  {
+    "revision": "2c6b6551e1a3895ab044",
+    "url": "/Pomodoro-Redux/static/js/2.8d744b23.chunk.js"
+  },
+  {
+    "revision": "aae0757f3c3679f699f5d39bc18f0dca",
+    "url": "/Pomodoro-Redux/static/js/2.8d744b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cc7635c6b6997750031",
+    "url": "/Pomodoro-Redux/static/js/main.9dc881dc.chunk.js"
+  },
+  {
+    "revision": "c9ef865d5e9ea0a6c8e6",
+    "url": "/Pomodoro-Redux/static/js/runtime-main.d6d7f664.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomodoro-Redux/static/media/bell.525320df.flac"
+  }
+]);
